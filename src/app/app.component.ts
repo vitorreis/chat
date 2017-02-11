a4dc359a3b18fe8f45a89a5cc037ceca0d0546a8
Vitor Reis <v.reis@foodpanda.com>
2017-02-11 16:56:07 +0100

    implement features with material design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,20 @@
 import { Component } from '@angular/core';
 
 const USERS: User[] = [
-  { id: 11, name: 'Mr. Nice', username: 'xpto' },
-  { id: 12, name: 'Narco', username: 'xpto' },
-  { id: 13, name: 'Bombasto', username: 'xpto' },
-  { id: 14, name: 'Celeritas', username: 'xpto' },
-  { id: 15, name: 'Magneta', username: 'xpto' },
-  { id: 16, name: 'RubberMan', username: 'xpto' },
-  { id: 17, name: 'Dynama', username: 'xpto' },
-  { id: 18, name: 'Dr IQ', username: 'xpto' },
-  { id: 19, name: 'Magma', username: 'xpto' },
-  { id: 20, name: 'Tornado', username: 'xpto' }
+  { id: 11, name: 'Mr. Nice', username: 'xpto', chat: {messages: []} },
+  { id: 12, name: 'Narco', username: 'xpto', chat: {messages: []} },
+  { id: 13, name: 'Bombasto', username: 'xpto', chat: {messages: []} },
+  { id: 14, name: 'Celeritas', username: 'xpto', chat: {messages: []} },
+  { id: 15, name: 'Magneta', username: 'xpto', chat: {messages: []} },
+  { id: 16, name: 'RubberMan', username: 'xpto', chat: {messages: []} }
 ];
 
 const MESSAGES: Message[] = [
-  { content: 'Hello!', fromUserId: 1, toUserId: 2},
-  { content: 'How you doing??', fromUserId: 1, toUserId: 2},
-  { content: 'Good morning! I am doing great, I am at the airport now, traveling to Barcelona.', fromUserId: 2, toUserId: 1},
-  { content: 'Cool! So it worked out uh?! Enjoy your holidays', fromUserId: 1, toUserId: 2},
-  { content: 'I will see you next week!', fromUserId: 2, toUserId: 1}
+  { content: 'Hello!', fromUserId: 1, toUserId: 12},
+  { content: 'How you doing??', fromUserId: 1, toUserId: 12},
+  { content: 'Good morning! I am doing great, I am at the airport now, traveling to Barcelona.', fromUserId: 12, toUserId: 1},
+  { content: 'Cool! So it worked out uh?! Enjoy your holidays', fromUserId: 1, toUserId: 12},
+  { content: 'I will see you next week!', fromUserId: 12, toUserId: 1}
 ];
 
 @Component({
@@ -29,8 +25,9 @@ const MESSAGES: Message[] = [
 export class AppComponent {
   title = 'Welcome to elite chat!';
   users = USERS;
-  chat = new Chat();
   currentUser = new User();
+  chattingWithUser = new User();
+  message = '';
 
 
   startChatting(): void {
@@ -38,7 +35,15 @@ export class AppComponent {
   }
 
   sendMessage() {
+    if (this.message) {
+      let message = new Message();
+      message.content = this.message;
+      message.fromUserId = this.currentUser.id;
+      message.toUserId = this.chattingWithUser.id;
 
+      this.chattingWithUser.chat.messages.push(message);
+      this.message = '';
+    }
   }
 }
 
@@ -46,6 +51,7 @@ class User {
   id: number;
   name: string;
   username: string;
+  chat: Chat = new Chat();
 }
 
 class Chat {
