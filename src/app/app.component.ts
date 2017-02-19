9ddb611a3a50a5a570c938da3f90d19411aa3eaf
Vitor Reis <v.reis@foodpanda.com>
2017-02-19 14:57:16 +0100

    design v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,39 +3,20 @@ import { Component } from '@angular/core';
 var SockJS = require('sockjs-client');
 var Stomp = require('stompjs');
 
-const USERS: User[] = [
-  { id: 11, name: 'Mr. Nice', chat: {messages: []} },
-  { id: 12, name: 'Narco', chat: {messages: []} },
-  { id: 13, name: 'Bombasto', chat: {messages: []} },
-  { id: 14, name: 'Celeritas', chat: {messages: []} },
-  { id: 15, name: 'Magneta', chat: {messages: []} },
-  { id: 16, name: 'RubberMan', chat: {messages: []} }
-];
-
-const MESSAGES: Message[] = [
-  { content: 'Hello!', fromUserId: 1, toUserId: 12, sentAt: null, isRead: false },
-  { content: 'How you doing??', fromUserId: 1, toUserId: 12, sentAt: null, isRead: false },
-  { content: 'Good morning! I am doing great, I am at the airport now, traveling to Barcelona.', fromUserId: 12, toUserId: 1, sentAt: null, isRead: false },
-  { content: 'Cool! So it worked out uh?! Enjoy your holidays', fromUserId: 1, toUserId: 12, sentAt: null, isRead: false },
-  { content: 'I will see you next week!', fromUserId: 12, toUserId: 1, sentAt: null, isRead: false }
-];
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isChatOpen = false;
   title = 'Welcome to elite chat!';
-  users = USERS;
   currentUser = new User();
-  chattingWithUser = new User();
   message = '';
   stompClient: any = null;
-
+  messageHistory: Message[] = [];
 
   startChatting(): void {
-    this.currentUser.id = 1;
     this.connect();
   }
 
@@ -43,13 +24,10 @@ export class AppComponent {
     if (this.message) {
       let message = new Message();
       message.content = this.message;
-      message.fromUserId = this.currentUser.id;
-      message.toUserId = this.chattingWithUser.id;
+      message.sentBy = this.currentUser.name;
 
-      this.chattingWithUser.chat.messages.push(message);
+      this.stompClient.send("/app/chat", {}, JSON.stringify(message));
       this.message = '';
-
-      this.stompClient.send("/app/chat", {}, JSON.stringify({ 'content': "kitica", 'sentBy': "Vitor" }))
     }
   }
 
@@ -61,9 +39,11 @@ export class AppComponent {
     console.log('Stomp client', this.stompClient);
     this.stompClient.connect({}, (frame) => {
       console.log('Connected: ' + frame);
-      this.stompClient.subscribe('/topic/public-chat', (greeting: any) => {
-        console.log(greeting);
-        //showGreeting(JSON.parse(greeting.body).content);
+      this.isChatOpen = true;
+      this.stompClient.subscribe('/topic/public-chat', (messageCommand: any) => {
+        console.log(JSON.parse(messageCommand.body));
+        let message = JSON.parse(messageCommand.body);
+        this.messageHistory.push(message);
       });
     });
   }
@@ -72,16 +52,11 @@ export class AppComponent {
 class User {
   id: number;
   name: string;
-  chat: Chat = new Chat();
-}
-
-class Chat {
-  messages: Message[] = MESSAGES;
 }
 
 class Message {
   content: string;
-  fromUserId: number;
+  sentBy: string;
   toUserId: number;
   sentAt: any;
   isRead: boolean = false;
